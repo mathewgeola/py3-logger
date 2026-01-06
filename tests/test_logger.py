37281a8e033c5127145881ebfecd3e34ba5061fe
mathewgeola <mathewgeola@gmail.com>
2026-01-06 13:13:20 +0800

    version = "v0.0.6"

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -1,16 +1,16 @@
-import logger
+import py3_logger
 
 
 def test_logger() -> None:
-    _logger = logger.get_logger(__name__, to_console=True, to_file=True, file_max_bytes=10, file_backup_count=3)
+    logger = py3_logger.get_logger(__name__, to_console=True, to_file=True, file_max_bytes=10, file_backup_count=3)
 
-    _logger.debug("debug message")
-    _logger.info("info message")
-    _logger.success("success message")
-    _logger.warning("warning message")
-    _logger.error("error message")
-    _logger.critical("critical message")
-    _logger.framework("framework message")
+    logger.debug("debug message")
+    logger.info("info message")
+    logger.success("success message")
+    logger.warning("warning message")
+    logger.error("error message")
+    logger.critical("critical message")
+    logger.framework("framework message")
 
 
 if __name__ == '__main__':
